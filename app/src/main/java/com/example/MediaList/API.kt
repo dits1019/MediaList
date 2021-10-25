58988ff54aa1065feaa813796d8e5442f85a4494
dits1019 <jdits1019@gmail.com>
2021-10-25 10:54:12 +0900

    평점이 높은 영화들 목록 추가

--- app/src/main/java/com/example/MediaList/API.kt
+++ app/src/main/java/com/example/MediaList/API.kt
@@ -6,8 +6,16 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface API {
+    // 인기있는 영화
     @GET("movie/popular")
     fun getPopularMovies(
+        @Query("api_key") apiKey: String = "7dac146b5390123d5d5b2665f0c5e8d6",
+        @Query("page") page: Int
+    ) : Call<GetMoviesResponse>
+
+    // 평점이 높은 영화화
+   @GET("movie/top_rated")
+    fun getTopRatedMovies(
         @Query("api_key") apiKey: String = "7dac146b5390123d5d5b2665f0c5e8d6",
         @Query("page") page: Int
     ) : Call<GetMoviesResponse>
--- app/src/main/java/com/example/MediaList/MainActivity.kt
+++ app/src/main/java/com/example/MediaList/MainActivity.kt
@@ -15,33 +15,62 @@ class MainActivity : AppCompatActivity() {
     private lateinit var popularMoviesAdapter: MoviesAdapter
     private lateinit var popularMoviesLayoutManager: LinearLayoutManager
 
+    private lateinit var topRatedMovies: RecyclerView
+    private lateinit var topRatedMoviesAdapter: MoviesAdapter
+    private lateinit var topRatedMoviesLayoutManager: LinearLayoutManager
+
+
     private var popularMoviesPage = 1
+    private var topRatedMoviesPage = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         popularMovies = findViewById(R.id.popular_movies)
-        popularMoviesLayoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        popularMoviesLayoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
         popularMovies.layoutManager = popularMoviesLayoutManager
-
         popularMoviesAdapter = MoviesAdapter(mutableListOf())
         popularMovies.adapter = popularMoviesAdapter
 
-        // 더블콜론(::)을 사용하면 변수에 대한 속성을 참조할 수 있음
-        // 함수 내에서 변수는 더블콜론으로 명시하여 참조 X
-        // 전역 변수나 클래스 등을 더블콜론으로 명시하여 참조 후 사용
+        topRatedMovies = findViewById(R.id.top_rated_movies)
+        topRatedMoviesLayoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        topRatedMovies.layoutManager = topRatedMoviesLayoutManager
+        topRatedMoviesAdapter = MoviesAdapter(mutableListOf())
+        topRatedMovies.adapter = topRatedMoviesAdapter
+
 
         getPopularMovies()
+        getTopRatedMovies()
 
     }
 
     private fun getPopularMovies() {
+        // 더블콜론(::)을 사용하면 변수에 대한 속성을 참조할 수 있음
+        // 함수 내에서 변수는 더블콜론으로 명시하여 참조 X
+        // 전역 변수나 클래스 등을 더블콜론으로 명시하여 참조 후 사용
         MoviesRepository.getPopularMovies(
             onSuccess = ::onPopularMoviesFetched,
             onError = ::onError
         )
     }
+
+    private fun getTopRatedMovies() {
+        MoviesRepository.getTopRatedMovies(
+            topRatedMoviesPage,
+            ::onTopRatedMoviesFetched,
+            ::onError
+        )
+    }
+
 
     private fun attachPopularMoviesOnScrollListener() {
         popularMovies.addOnScrollListener(object : RecyclerView.OnScrollListener() {
@@ -59,10 +88,33 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    private fun attachTopRatedMoviesOnScrollListener() {
+        topRatedMovies.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val totalItemCount = topRatedMoviesLayoutManager.itemCount
+                val visibleItemCount = topRatedMoviesLayoutManager.childCount
+                val firstVisibleItem = topRatedMoviesLayoutManager.findFirstVisibleItemPosition()
+
+                if (firstVisibleItem + visibleItemCount >= totalItemCount / 2) {
+                    topRatedMovies.removeOnScrollListener(this)
+                    topRatedMoviesPage++
+                    getTopRatedMovies()
+                }
+            }
+        })
+    }
+
+
     private fun onPopularMoviesFetched(movies: List<Movie>) {
         popularMoviesAdapter.appendMovies(movies)
         attachPopularMoviesOnScrollListener()
     }
+
+    private fun onTopRatedMoviesFetched(movies: List<Movie>) {
+        topRatedMoviesAdapter.appendMovies(movies)
+        attachTopRatedMoviesOnScrollListener()
+    }
+
 
     private fun onError() {
         Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
